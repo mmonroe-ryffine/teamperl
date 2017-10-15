922bc49ef93f0bcee735c85ee4c109ab72536e00
mmonroe-ryffine <micaela.monroe@ryffine.com>
2017-10-15 15:34:24 -0700

    printed ARGV[0] and [1] on different lines

--- helloWorld.pl
+++ helloWorld.pl
@@ -1,4 +1,9 @@
 #!/usr/bin/perl
 
-my $name = $ARGV[-1];
-say STDERR "Hello $name";
+my $name1 = $ARGV[0];
+my $name2 = $ARGV[1];
+say STDERR "Hello $name1";
+say STDERR "Hello $name2";
+
+#use Data::Dump qw(dump);
+#
